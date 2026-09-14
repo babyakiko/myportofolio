dfbf1ad71c69173c3b3727a1f478afe3e6335d01
babyakiko <baby.akiko@ui.ac.id>
2026-09-15 01:42:37 +0700

    Add migration for project ID and fix settings

--- main/models.py
+++ main/models.py
@@ -33,6 +33,7 @@ class Experience(models.Model):
         return self.ended_at is None
 
 class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     role = models.CharField(max_length=255)
     description = models.TextField()
